82aac72eb360695957be35d9f2463d4cbe9f7abe
qiupansi <1039355704@qq.com>
2020-06-21 16:52:28 +0800

    First Commit

---
+++ app/src/main/java/com/psqiu/looplayout/LauncherBean.java
@@ -0,0 +1,48 @@
+package com.psqiu.looplayout;
+
+public class LauncherBean {
+
+    private int mAnim;
+    private int mFronts;
+    private int mImages;
+    private String mIconName;
+
+    public LauncherBean(int anim, int fronts, int images, String iconName) {
+        mAnim = anim;
+        mFronts = fronts;
+        mImages = images;
+        mIconName = iconName;
+    }
+
+    public int getAnim() {
+        return mAnim;
+    }
+
+    public void setAnim(int anim) {
+        mAnim = anim;
+    }
+
+    public int getFronts() {
+        return mFronts;
+    }
+
+    public void setFronts(int fronts) {
+        mFronts = fronts;
+    }
+
+    public int getImages() {
+        return mImages;
+    }
+
+    public void setImages(int images) {
+        mImages = images;
+    }
+
+    public String getIconName() {
+        return mIconName;
+    }
+
+    public void setIconName(String iconName) {
+        mIconName = iconName;
+    }
+}
